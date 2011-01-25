46cd3dc0d919e3dd4f1d5ea501705033fe5799f6
Theodore Robert Campbell Jr <trcjr@stupidfoot.com>
2011-01-24 23:23:07 -0500

    can now create html suites

--- lib/WWW/Selenium/Selenese/TestCase.pm
+++ lib/WWW/Selenium/Selenese/TestCase.pm
@@ -15,6 +15,10 @@ use WWW::Selenium::Selenese::Command;
 use Text::MicroTemplate;
 use File::Basename;
 
+use Data::Dumper;
+$Data::Dumper::Indent = 1;
+use HTML::Element;
+
 sub case_to_perl {
     __PACKAGE__->new(shift)->as_perl;
 }
@@ -120,6 +124,73 @@ sub convert_to_perl {
     close $io;
 
     return $outfile;
+}
+
+sub as_HTML {
+    my $self = shift;
+
+    my $title_text = "this is the title text";
+
+    my $root  = HTML::TreeBuilder->new();
+    my $head  = $root->find('head');
+    my $table = HTML::Element->new(
+        'table',
+        cellpadding => 1,
+        cellspacing => 1,
+        border      => 1
+    );
+    my $meta = HTML::Element->new(
+        'meta',
+        'http-equiv' => "Content-Type",
+        content      => "text/html; charset=UTF-8"
+    );
+
+    my $link = HTML::Element->new(
+        'link',
+        rel  => "selenium.base",
+        href => "http://www.google.com/"
+    );
+
+    $root->find('body')->insert_element($table);
+    my $thead = HTML::Element->new('thead');
+    my $tr    = HTML::Element->new('tr');
+    my $td    = HTML::Element->new( 'td', rowspan => 1, colspan => 3 );
+    my $tbody = HTML::Element->new('tbody');
+
+    $tr->insert_element($td);
+    $td->unshift_content($title_text);
+
+    $head->attr( 'profile',
+        "http://selenium-ide.openqa.org/profiles/test-case" );
+
+    $head->insert_element($meta);
+    $head->insert_element($link);
+    $thead->insert_element($tr);
+    $table->push_content($thead);
+    $table->push_content($tbody);
+    foreach my $command ( @{ $self->{commands} } ) {
+        my $element;
+        if ( $command->{values}->[0] eq "comment" ) {
+
+            $element = HTML::Element->new('~comment');
+            $element->attr( 'text', $command->{values}->[1] );
+
+        }
+        else {
+            $element = HTML::Element->new('tr');
+            foreach my $value ( @{ $command->{values} } ) {
+                my $td = HTML::Element->new('td')->unshift_content($value);
+                $element->push_content($td);
+            }
+        }
+        $tbody->push_content($element);
+    }
+    open my $f, ">", "s.html";
+    print $f $root->as_XML;
+    close($f);
+
+    return $root->as_XML;
+
 }
 
 1;
